cf3f5fd7d502a7c51564994594226280d5e77419
Nathan Hamblen <nathan@technically.us>
2012-01-21 14:22:27 -0500

    addListener interface so that foreach can be centrally defined

--- src/main/scala/promise.scala
+++ src/main/scala/promise.scala
@@ -14,42 +14,41 @@ trait Promise[+A] extends Function0[A] { self =>
         if (p(r)) r
         else throw new java.util.NoSuchElementException("Empty Promise.get")
       }
-      def foreach(f: (=> A) => Unit) =
-        for (a <- self)
-          if (p(a)) f(a)
+      /** Listener only invoked if predicate is met, otherwise exception
+       * will be thrown by Promise#foreach */
+      def addListener(f: () => Unit) {
+        self.addListener { () =>
+          if (p(self.get))
+            f()
+        } 
+      }
     }
   def map[B](f: A => B) =
     new Promise[B] {
       def get = f(self.get)
-      def foreach(f2: (=> B) => Unit) =
-        for (a <- self)
-          f2(f(a))
+      def addListener(f: () => Unit) { self.addListener(f) }
     }
   def flatMap[B, C, That <: Promise[C]]
              (f: A => B)
              (implicit guarantor: Guarantor[B,C,That]) =
     new Promise[C] {
       def get = guarantor.promise(f(self.get)).get
-      def foreach(f2: (=> C) => Unit) =
-        for {
-          a <- self
-          b <- guarantor.promise(f(a))
-        } f2(b)
+      def addListener(f: () => Unit) { self.addListener(f) }
     }
   /** Promise to asynchronously cause some side effect, by-name of A so
    *  that f can catch exceptions thrown while getting it */
-  def foreach(f: (=> A) => Unit)
+  def foreach(f: A => Unit) {
+    addListener { () => f(get) }
+  }
+
+  def addListener(f: () => Unit)
 
   def either =
     new Promise[Either[Throwable,A]] {
       // retain references to eithers since exceptions are only thrown once
       lazy val get = allCatch.either { self.get }
 
-      def foreach(f: (=> Either[Throwable,A]) => Unit) =
-        for ( a <- self) {
-          val e = allCatch.either { a }
-          f(e)
-        }
+      def addListener(f: () => Unit) { self.addListener(f) }
     }
 
   def option: Promise[Option[A]] =
@@ -61,10 +60,10 @@ class ListenableFuturePromise[A](
   executor: juc.Executor
 ) extends Promise[A] {
   def get = underlying.get
-  def foreach(f: (=> A) => Unit) =
+  def addListener(f: () => Unit) =
     underlying.addListener(new Runnable {
       def run {
-        f(get)
+        f()
       }
     }, executor)
 }
@@ -76,12 +75,15 @@ object Promise {
 
   def all[A](promises: Traversable[Promise[A]]) =
     new Promise[Traversable[A]] { self =>
-      def get = promises.map { _.get }
-      def foreach(f: (=> Traversable[A]) => Unit) = {
+      lazy val get = promises.map { _.get }
+      def addListener(f: () => Unit) = {
         val count = new juc.atomic.AtomicInteger(promises.size)
-        for (p <- promises; a <- p)
-          if (count.decrementAndGet == 0)
-            f(self.get)
+        promises.foreach { p =>
+          p.addListener { () =>
+            if (count.decrementAndGet == 0)
+              f()
+          }
+        }
       }
     }
   implicit def traversable[T] = new TraversableGuarantor[T]
