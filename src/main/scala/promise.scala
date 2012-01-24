234515d008bdc0358dd279819edf3379f80e04a1
Nathan Hamblen <nathan@technically.us>
2012-01-23 23:23:31 -0500

    add listener all promises

--- src/main/scala/promise.scala
+++ src/main/scala/promise.scala
@@ -5,48 +5,51 @@ import java.util.{concurrent => juc}
 import scala.util.control.Exception.allCatch
 
 trait Promise[+A] extends Function0[A] { self =>
-  def apply() = get
-  def get: A
+  private lazy val result = allCatch.either { claim }
+  def apply() = result.fold(
+    exc => throw(exc),
+    identity
+  )
+  protected def claim: A
   def filter(p: A => Boolean) =
     new Promise[A] {
-      def get = {
-        val r = self.get
+      def claim = {
+        val r = self()
         if (p(r)) r
-        else throw new java.util.NoSuchElementException("Empty Promise.get")
+        else throw new java.util.NoSuchElementException("Empty Promise")
       }
       /** Listener only invoked if predicate is met, otherwise exception
        * will be thrown by Promise#foreach */
       def addListener(f: () => Unit) {
         self.addListener { () =>
-          if (p(self.get))
+          if (p(self()))
             f()
         } 
       }
     }
   def map[B](f: A => B) =
     new Promise[B] {
-      def get = f(self.get)
+      def claim = f(self())
       def addListener(f: () => Unit) { self.addListener(f) }
     }
   def flatMap[B, C, That <: Promise[C]]
              (f: A => B)
              (implicit guarantor: Guarantor[B,C,That]) =
     new Promise[C] {
-      def get = guarantor.promise(f(self.get)).get
+      def claim = guarantor.promise(f(self()))()
       def addListener(f: () => Unit) { self.addListener(f) }
     }
-  /** Promise to asynchronously cause some side effect, by-name of A so
-   *  that f can catch exceptions thrown while getting it */
+  /** Promise to asynchronously cause some side effect */
   def foreach(f: A => Unit) {
-    addListener { () => f(get) }
+    addListener { () => f(self()) }
   }
 
   def addListener(f: () => Unit)
+  addListener { () => result }
 
   def either =
     new Promise[Either[Throwable,A]] {
-      // retain references to eithers since exceptions are only thrown once
-      lazy val get = allCatch.either { self.get }
+      def claim = self.result
 
       def addListener(f: () => Unit) { self.addListener(f) }
     }
@@ -59,7 +62,7 @@ class ListenableFuturePromise[A](
   underlying: ListenableFuture[A],
   executor: juc.Executor
 ) extends Promise[A] {
-  def get = underlying.get
+  def claim = underlying.get
   def addListener(f: () => Unit) =
     underlying.addListener(new Runnable {
       def run {
@@ -75,7 +78,7 @@ object Promise {
 
   def all[A](promises: Traversable[Promise[A]]) =
     new Promise[Traversable[A]] { self =>
-      lazy val get = promises.map { _.get }
+      def claim = promises.map { _() }
       def addListener(f: () => Unit) = {
         val count = new juc.atomic.AtomicInteger(promises.size)
         promises.foreach { p =>
