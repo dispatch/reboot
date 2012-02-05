58ef7c8491f08a8efce7e7f3e0225443c5756bb7
Nathan Hamblen <nathan@technically.us>
2012-02-05 12:20:32 -0500

    futures & promises SIP methods

--- src/main/scala/promise.scala
+++ src/main/scala/promise.scala
@@ -4,14 +4,47 @@ import com.ning.http.client.ListenableFuture
 import java.util.{concurrent => juc}
 import scala.util.control.Exception.allCatch
 
-trait Promise[+A] extends Function0[A] { self =>
-  private lazy val result = allCatch.either { claim }
+trait Promise[+A] extends Function0[A] with PromiseSIP[A] { self =>
+  /** Claim promise or throw exception, should only be called once */
+  protected def claim: A
+
+  /** Internal cache of promised value or exception thrown */
+  protected lazy val result = allCatch.either { claim }
+
+  /** Listener to be called in an executor when promise is available */
+  protected def addListener(f: () => Unit)
+
+  // lazily assign result when available, e.g. to evaluate mapped function
+  // that may kick off further promises
+  addListener { () => result }
+
+  /** Blocks until promised value is available, returns promised value or throws
+   *  its associated exception. */
   def apply() = result.fold(
     exc => throw(exc),
     identity
   )
-  protected def claim: A
-  def filter(p: A => Boolean) =
+  /** Nested promise that delegates listening directly to this self */
+  protected trait SelfPromise[+A] extends Promise[A] {
+      def addListener(f: () => Unit) { self.addListener(f) }
+  }
+  /** Map the promised value to something else */
+  def map[B](f: A => B): Promise[B] =
+    new SelfPromise[B] {
+      def claim = f(self())
+    }
+  /** Bind this Promise to another Promise, or something which an
+   *  implicit Guarantor may convert to a Promise. */
+  def flatMap[B, C, That <: Promise[C]]
+             (f: A => B)
+             (implicit guarantor: Guarantor[B,C,That]): Promise[C] =
+    new SelfPromise[C] {
+      def claim = guarantor.promise(f(self()))()
+    }
+  /** Support if clauses in for expressions. A filtered promise
+   *  behaves like an Option, in that apply() will throw a
+   *  NoSuchElementException when the promise is empty. */
+  def filter(p: A => Boolean): Promise[A] =
     new Promise[A] {
       def claim = {
         val r = self()
@@ -27,35 +60,55 @@ trait Promise[+A] extends Function0[A] { self =>
         } 
       }
     }
-  def map[B](f: A => B) =
-    new Promise[B] {
-      def claim = f(self())
-      def addListener(f: () => Unit) { self.addListener(f) }
-    }
-  def flatMap[B, C, That <: Promise[C]]
-             (f: A => B)
-             (implicit guarantor: Guarantor[B,C,That]) =
-    new Promise[C] {
-      def claim = guarantor.promise(f(self()))()
-      def addListener(f: () => Unit) { self.addListener(f) }
-    }
-  /** Promise to asynchronously cause some side effect */
+  /** Cause some side effect with the promised value, if it is produced
+   *  with no exception */
   def foreach(f: A => Unit) {
     addListener { () => f(self()) }
   }
 
-  def addListener(f: () => Unit)
-  addListener { () => result }
-
-  def either =
-    new Promise[Either[Throwable,A]] {
+  /** Project promised value into an either containing the value or any
+   *  exception thrown retrieving it */
+  def either: Promise[Either[Throwable, A]] =
+    new SelfPromise[Either[Throwable, A]] {
       def claim = self.result
-
-      def addListener(f: () => Unit) { self.addListener(f) }
     }
 
+  /** Project promised value into an Option containing the value if retrived
+   *  with no exception */
   def option: Promise[Option[A]] =
     either.map { _.right.toOption }
+}
+
+trait PromiseSIP[+A] { self: Promise[A] =>
+  def onComplete[U](f: Either[Throwable, A] => U) = {
+    for (e <- either) f(e)
+    self
+  }
+
+  def onSuccess[U](f: PartialFunction[A, U]) = {
+    for {
+      p <- self
+      _ <- f.lift(p)
+    } ()
+    self
+  }
+
+  def onFailure[U](f: PartialFunction[Throwable, U]) = {
+    for {
+      e <- either
+      t <- e.left
+      _ <- f.lift(t)
+    } ()
+    self
+  }
+
+  def recover[B >: A](f: PartialFunction[Throwable, B]): Promise[B] =
+    new SelfPromise[B] {
+      def claim = self.result.fold(
+        t => f.lift(t).getOrElse { throw t },
+        identity
+      )
+    }
 }
 
 class ListenableFuturePromise[A](
