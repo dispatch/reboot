9ad116d30857657bd65de8f7ee46e78a3fce199a
Nathan Hamblen <nathan@technically.us>
2012-06-28 00:31:03 -0400

    streaming api accessor

---
+++ core/src/main/scala/stream.scala
@@ -0,0 +1,56 @@
+package dispatch
+
+import util.control.Exception._
+
+import scala.collection.JavaConverters._
+import com.ning.http.client._
+import com.ning.http.util.AsyncHttpProviderUtils.parseCharset
+
+trait StreamString[T] extends AsyncHandler[T] {
+  private var charset = "utf-8"
+  def onThrowable(t: Throwable) { }
+  def onCompleted(): T
+  def onStatusReceived(status: HttpResponseStatus) =
+    AsyncHandler.STATE.CONTINUE
+  def onHeadersReceived(headers: HttpResponseHeaders) = {
+    for {
+      ct <- headers.getHeaders.get("content-type").asScala.headOption
+      cs <- Option(parseCharset("charset"))
+    } charset = cs
+    AsyncHandler.STATE.CONTINUE
+  }
+  def onString(str: String)
+  def onBodyPartReceived(bodyPart: HttpResponseBodyPart) = {
+    onString(new String(bodyPart.getBodyPartBytes, charset))
+    AsyncHandler.STATE.CONTINUE
+  }
+}
+
+trait StreamStringBy[T] extends StreamString[T] {
+  @volatile private var buffer = ""
+
+  def divider: String
+
+  def onStringBy(string: String)
+
+  def onString(string: String) {
+    val strings = (buffer + string).split(divider, -1)
+    strings.take(strings.length - 1).filter { !_.isEmpty }.foreach(onStringBy)
+    buffer = strings.last
+  }
+}
+
+trait StreamStringByLine[T] extends StreamStringBy[T] {
+  def divider = "[\n\r]+"
+}
+
+object AsStream {
+  def lines[T](f: String => T) =
+    new StreamStringByLine[T] {
+      @volatile private var last: T = _
+      def onStringBy(string: String) {
+        last = f(string)
+      }
+      def onCompleted = last
+    }
+}
