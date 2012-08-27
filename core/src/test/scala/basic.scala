fe6d68962c541322bf94f59308b99c87f2c5b7d3
Nathan Hamblen <nathan@technically.us>
2012-08-27 11:40:36 -0400

    check encoded characters in request paths

--- core/src/test/scala/basic.scala
+++ core/src/test/scala/basic.scala
@@ -5,7 +5,8 @@ import org.scalacheck._
 object BasicSpecification
 extends Properties("Basic")
 with unfiltered.spec.ServerCleanup {
-  import Prop.forAll
+  import java.net.{URLEncoder,URLDecoder}
+  import Prop._
 
   val server = {
     import unfiltered.netty
@@ -15,6 +16,10 @@ with unfiltered.spec.ServerCleanup {
     netty.Http.anylocal.handler(netty.cycle.Planify {
       case req @ Path("/echo") & Params(Echo(echo)) =>
         PlainTextContent ~> ResponseString(req.method + echo)
+      case req @ Path(Seg("echopath" :: echo :: _)) =>
+        PlainTextContent ~> ResponseString(req.method + URLDecoder.decode(echo, "utf-8"))
+      case req @ Path(Seg("echopath" :: Nil)) =>
+        PlainTextContent ~> ResponseString(req.method)
     }).start()
   }
 
@@ -22,31 +27,48 @@ with unfiltered.spec.ServerCleanup {
 
   def localhost = host("127.0.0.1", server.port)
 
+  // a shim until we can update scalacheck to a version that non-alpha strings that don't break Java
+  val syms = "&#$@%"
+
   property("POST and handle") = forAll(Gen.alphaStr) { (sample: String) =>
     val res = Http(
       localhost / "echo" << Map("echo" -> sample) > as.String
     )
-    res() == ("POST" + sample)
+    res() =? ("POST" + sample)
   }
 
   property("GET and handle") = forAll(Gen.alphaStr) { (sample: String) =>
     val res = Http(
       localhost / "echo" <<? Map("echo" -> sample) > as.String
     )
-    res() == ("GET" + sample)
+    res() =? ("GET" + sample)
   }
 
   property("GET and get response") = forAll(Gen.alphaStr) { (sample: String) =>
     val res = Http(
       localhost / "echo" <<? Map("echo" -> sample)
     )
-    res().getResponseBody == ("GET" + sample)
+    res().getResponseBody =? ("GET" + sample)
+  }
+
+  property("GET with encoded path") = forAll(Gen.alphaStr) { (sample: String) =>
+    // (second sample in request path is ignored)
+    val res = Http(
+      localhost / "echopath" / (sample + syms) / sample OK as.String
+    )
+    ("GET" + sample + syms) =? res()
+  }
+
+  property("GET with encoded path as url") = forAll(Gen.alphaStr) { (sample: String) =>
+    val requesturl = "http://127.0.0.1:%d/echopath/%s".format(server.port, URLEncoder.encode(sample + syms, "utf-8"))
+    val res = Http(url(requesturl) / sample OK as.String)
+    res() == ("GET" + sample + syms)
   }
 
   property("OPTIONS and handle") = forAll(Gen.alphaStr) { (sample: String) =>
     val res = Http(
       localhost.OPTIONS / "echo" <<? Map("echo" -> sample) > as.String
     )
-    res() == ("OPTIONS" + sample)
+    res() =? ("OPTIONS" + sample)
   }
 }
