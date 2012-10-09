dfacf4eecea8e37b8944775967ca4fd7aab38ac3
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-10-08 19:22:56 -0700

    Update to latest version of json4s

--- json4snative/src/main/scala/as/json.scala
+++ json4snative/src/main/scala/as/json.scala
@@ -6,5 +6,5 @@ import com.ning.http.client.Response
 
 object Json extends (Response => JValue) {
   def apply(r: Response) =
-    (dispatch.as.String andThen parse)(r)
+    (dispatch.as.String andThen (s => parse(StringInput(s), true)))(r)
 }
