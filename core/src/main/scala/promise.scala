3e333261b61d3741ea1d76125c4fd4dd9bf9b0e0
Nathan Hamblen <nathan@technically.us>
2012-10-14 18:33:25 -0400

    atomic operations, careful racing

--- core/src/main/scala/promise.scala
+++ core/src/main/scala/promise.scala
@@ -164,24 +164,32 @@ object Promise {
 
   def sleep[T](d: Duration)(todo: => T) = new Promise[T] { self =>
     import org.jboss.netty.util.{TimerTask, Timeout}
-    // put countdown latch in claim
-    def claim = todo
+    private val latch = new juc.CountDownLatch(1)
+    def claim = {
+      latch.await()
+      todo
+    }
     // note: super calls addListener before listeners can be initialized
-    var listeners: List[(() => Unit)] = _
+    private val listeners =
+      new juc.atomic.AtomicReference(List.empty[(() => Unit)])
     val sleepTimeout = Http.timer.newTimeout(new TimerTask {
       def run(timeout: Timeout) {
-        for (listener <- listeners) listener()
+        latch.countDown()
+        val ls = listeners.getAndSet(Nil)
+        for (l <- ls) l()
       }
     }, d.length, d.unit)
-    def isComplete = sleepTimeout.isExpired
+    def isComplete = latch.getCount == 0
     val timeout = d
-    def addListener(f: () => Unit) = {
-      listeners = (
-        if (listeners == null)
-          f :: Nil
-        else
-          f :: listeners
-      )
+    def addListener(f: () => Unit) {
+      val ls = listeners.get
+      if (isComplete) {
+        f() // should really happen in a background thread
+      } else {
+        if (! listeners.compareAndSet(ls, f :: ls)) {
+          addListener(f)
+        }
+      }
     }
   }
     
