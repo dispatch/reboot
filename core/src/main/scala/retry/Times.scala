bc59dde4590f21a27348a10776bf1bc9f8c29062
Nathan Hamblen <nathan@technically.us>
2012-10-21 22:53:18 -0400

    retry with pause

--- core/src/main/scala/retry/Times.scala
+++ core/src/main/scala/retry/Times.scala
@@ -2,12 +2,24 @@ package dispatch.retry
 
 import dispatch._
 
-object Times {
+object Directly {
   def apply[T](count: Int)(promise: Promise[T])
               (implicit success: Success[T]): Promise[T] = {
     promise.flatMap { res =>
       if (count < 1 || success.predicate(res)) promise
-      else Times(count - 1)(promise.replay)
+      else Directly(count - 1)(promise.replay)
+    }
+  }
+}
+
+object Pause {
+  def apply[T](count: Int, delay: Duration)(promise: Promise[T])
+              (implicit success: Success[T]): Promise[T] = {
+    promise.flatMap { res =>
+      if (count < 1 || success.predicate(res)) promise
+      else promise.http.promise.sleep(delay) {
+        Pause(count - 1, delay)(promise.replay)
+      }.flatten
     }
   }
 }
