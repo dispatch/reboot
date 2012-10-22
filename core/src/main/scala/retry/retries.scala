5f45faea31caca1d3d2068311ae1b386fa367936
Nathan Hamblen <nathan@technically.us>
2012-10-21 23:40:39 -0400

    option success condition

--- core/src/main/scala/retry/retries.scala
+++ core/src/main/scala/retry/retries.scala
@@ -50,6 +50,8 @@ class Success[T](val predicate: T => Boolean)
 object Success {
   implicit def either[A,B]: Success[Either[A,B]] =
     new Success(_.isRight)
+  implicit def option[A]: Success[Option[A]] =
+    new Success(!_.isEmpty)
 }
 
 trait CountingRetry {
