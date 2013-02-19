3fe6d4c7d79d1630573d799c7686e814d9c6cf99
Adam Shannon <adam@ashannon.us>
2013-02-19 13:16:43 -0600

    Adding tests for the various params.

--- core/src/test/scala/immutable/operations.scala
+++ core/src/test/scala/immutable/operations.scala
@@ -4,6 +4,7 @@ import dispatch.as
 import org.scalacheck._
 import dispatch.BuildInfo
 import dispatch.immutable._
+import scala.collection.JavaConversions.JMapWrapper
 
 object OperationsSpecification extends Properties("Operations") {
   import Prop._
@@ -25,6 +26,42 @@ object OperationsSpecification extends Properties("Operations") {
     //res2 ?= ("http://localhost/path/path2")
   }
 
+  property("Adding headers") = forAll(Gen.alphaStr) { (sample: String) =>
+    val res = getHeaders(baseUrl <:< Map("header1" -> sample, "header2" -> "value2"))
+    res.find(_._1 == "header1") ?= Some("header1" -> sample)
+    res.find(_._1 == "header2") ?= Some("header2" -> "value2")
+  }
+
+  property("Removing headers") = forAll(Gen.alphaStr) { (sample: String) =>
+    val res = getHeaders((baseUrl <:< Map("header1" -> sample, "header2" -> "value2")).removeHeaders(Map("header2" -> "other")))
+    res.find(_._1 == "header1") ?= Some("header1" -> sample)
+    res.find(_._1 == "header2") ?= None
+  }
+
+  property("Adding Post Params") = forAll(Gen.alphaStr) { (sample: String) =>
+    val res = getPostParams(baseUrl << Map("param1" -> sample, "param2" -> "value2"))
+    res.find(_._1 == "param1") ?= Some("param1" -> sample)
+    res.find(_._1 == "param2") ?= Some("param2" -> "value2")
+  }
+
+  property("Removing Post Params") = forAll(Gen.alphaStr) { (sample: String) =>
+    val res = getPostParams((baseUrl << Map("param1" -> sample, "param2" -> "value2")).removePostParams(Map("param2" -> "other")))
+    res.find(_._1 == "param1") ?= Some("param1" -> sample)
+    res.find(_._1 == "param2") ?= None
+  }
+
+  property("Adding Query Params") = forAll(Gen.alphaStr) { (sample: String) =>
+    val res = getQueryParams(baseUrl <<? Map("param1" -> sample, "param2" -> "value2"))
+    res.find(_._1 == "param1") ?= Some("param1" -> sample)
+    res.find(_._1 == "param2") ?= Some("param2" -> "value2")
+  }
+
+  property("Removing Query Params") = forAll(Gen.alphaStr) { (sample: String) =>
+    val res = getQueryParams((baseUrl <<? Map("param1" -> sample, "param2" -> "value2")).removeQueryParams(Map("param2" -> "other")))
+    res.find(_._1 == "param1") ?= Some("param1" -> sample)
+    res.find(_._1 == "param2") ?= None
+  }
+
   property("Setting a url to secure") = forAll(Gen.alphaStr) { (sample: String) =>
     val req = baseUrl / "path" secure
     val res = getUrl(req)
@@ -34,4 +71,12 @@ object OperationsSpecification extends Properties("Operations") {
   }
 
   private[this] def getUrl(req: HttpRequest): String = req.request.build.getUrl()
+  private[this] def getHeaders(req: HttpRequest): Map[String, String] =
+    JMapWrapper(req.request.build.getHeaders()).toMap.map(pair => (pair._1 -> pair._2.get(0)))
+
+  private[this] def getPostParams(req: HttpRequest): Map[String, String] =
+    JMapWrapper(req.request.build.getParams()).toMap.map(pair => (pair._1 -> pair._2.get(0)))
+
+  private[this] def getQueryParams(req: HttpRequest): Map[String, String] =
+    JMapWrapper(req.request.build.getQueryParams()).toMap.map(pair => (pair._1 -> pair._2.get(0)))
 }
