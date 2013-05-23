cb7f3c90e457e875edf10564dde51ed42278e9ab
Nathan Hamblen <nathan@technically.us>
2013-05-23 17:26:50 -0400

    restructure defaults, make ugly daemon thread stuff private

--- core/src/main/scala/execution.scala
+++ core/src/main/scala/execution.scala
@@ -36,29 +36,77 @@ object Http extends Http(
 )
 
 private [dispatch] object InternalDefaults {
-  lazy val client = new AsyncHttpClient(config)
-  lazy val config = new AsyncHttpClientConfig.Builder()
-    .setUserAgent("Dispatch/%s" format BuildInfo.version)
-    .setAsyncHttpClientProviderConfig(
-      new NettyAsyncHttpProviderConfig().addProperty(
-        NettyAsyncHttpProviderConfig.SOCKET_CHANNEL_FACTORY,
-        nioClientSocketChannelFactory
-      )
-    ).setRequestTimeoutInMs(-1) // don't timeout streaming connections
-    .build
-  lazy val nioClientSocketChannelFactory = {
-    val workerCount = 2 * Runtime.getRuntime().availableProcessors()
-    new NioClientSocketChannelFactory(
-      juc.Executors.newCachedThreadPool(DaemonThreads.factory),
-      1,
-      new NioWorkerPool(
-        juc.Executors.newCachedThreadPool(DaemonThreads.factory),
-        workerCount
-      ),
-      timer
-    )
+  /** true if we think we're runing un-forked in an sbt-interactive session */
+  val inTrapExit = (
+    for (group <- Option(Thread.currentThread.getThreadGroup))
+    yield group.getName == "trap.exit"
+  ).getOrElse(false)
+
+  private lazy val underlying = 
+    if (inTrapExit) SbtProcessDefaults
+    else BasicDefaults
+
+  lazy val client = new AsyncHttpClient(underlying.builder.build())
+  lazy val timer = underlying.timer
+
+  private trait Defaults {
+    def builder: AsyncHttpClientConfig.Builder
+    def timer: Timer
   }
-  lazy val timer = new HashedWheelTimer(DaemonThreads.factory)
+
+  /** Sets a user agent, no timeout for requests  */
+  private object BasicDefaults extends Defaults {
+    lazy val timer = new HashedWheelTimer()
+    lazy val builder = new AsyncHttpClientConfig.Builder()
+      .setUserAgent("Dispatch/%s" format BuildInfo.version)
+      .setRequestTimeoutInMs(-1) // don't timeout streaming connections
+  }
+
+  /** Uses daemon threads and tries to exit cleanly when running in sbt  */
+  private object SbtProcessDefaults extends Defaults {
+    lazy val builder =
+      BasicDefaults.builder.setAsyncHttpClientProviderConfig(
+        new NettyAsyncHttpProviderConfig().addProperty(
+          NettyAsyncHttpProviderConfig.SOCKET_CHANNEL_FACTORY,
+          nioClientSocketChannelFactory
+        )
+      )
+
+    lazy val timer = new HashedWheelTimer(daemonThreadFactory)
+
+    lazy val nioClientSocketChannelFactory = {
+      val workerCount = 2 * Runtime.getRuntime().availableProcessors()
+      new NioClientSocketChannelFactory(
+        juc.Executors.newCachedThreadPool(daemonThreadFactory),
+        1,
+        new NioWorkerPool(
+          juc.Executors.newCachedThreadPool(daemonThreadFactory),
+          workerCount
+        ),
+        timer
+      )
+    }
+
+    private val shuttingDown = new juc.atomic.AtomicBoolean(false)
+    def shutdown() {
+      if (shuttingDown.compareAndSet(false, true)) {
+        nioClientSocketChannelFactory.releaseExternalResources()
+      }
+    }
+    /** produces daemon threads that shut down everything when interrupted! */
+    val daemonThreadFactory = new juc.ThreadFactory {
+      def newThread(runnable: Runnable): Thread ={
+        val thread = new Thread(runnable) {
+          override def interrupt() {
+            SbtProcessDefaults.shutdown()
+            super.interrupt()
+          }
+        }
+        thread.setDaemon(true)
+        thread
+      }
+    }
+  }
 }
 
 trait HttpExecutor { self =>
@@ -98,20 +146,7 @@ trait HttpExecutor { self =>
   }
 
   def shutdown() {
-    InternalDefaults.nioClientSocketChannelFactory.releaseExternalResources()
     client.close()
   }
 }
 
-object DaemonThreads {
-  /** produces daemon threads that won't block JVM shutdown */
-  val factory = new juc.ThreadFactory {
-    def newThread(runnable: Runnable): Thread ={
-      val thread = new Thread(runnable)
-      thread.setDaemon(true)
-      thread
-    }
-  }
-  def apply(threadPoolSize: Int) =
-    juc.Executors.newFixedThreadPool(threadPoolSize, factory)
-}
