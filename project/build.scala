56b946dd9d340c46d39d18dae2cf76cf31ebaac3
Nathan Hamblen <nathan@technically.us>
2014-05-04 15:08:30 -0400

    rm lift-json until a 2.11 artifact is available

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ object Builds extends sbt.Build {
         ls.Plugin.LsKeys.skipWrite := true,
       publish := { }
       )
-    ).aggregate(core, liftjson, jsoup, tagsoup, 
+    ).aggregate(core, jsoup, tagsoup, // liftjson - no 2.11 artifact
       json4sJackson, json4sNative)
 
   def module(name: String, settings: Seq[Def.Setting[_]] = Seq.empty) =
