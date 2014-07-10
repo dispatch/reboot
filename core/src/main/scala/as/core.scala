d69a4ebda9a6d51b342f7e3055c19ce2bb10c560
softprops <d.tangren@gmail.com>
2014-07-10 08:23:13 -0400

    provide interface that exposes ahc's interface for decoding the response body as a string in character sets other than the default (ISO-8859-1)

--- core/src/main/scala/as/core.scala
+++ core/src/main/scala/as/core.scala
@@ -3,13 +3,23 @@ package dispatch.as
 import dispatch._
 
 import com.ning.http.client
+import java.nio.charset.Charset
 
 object Response {
   def apply[T](f: client.Response => T) = f
 }
 
 object String extends (client.Response => String) {
+  /** @return response body as a ISO-8859-1 decoded string */
   def apply(r: client.Response) = r.getResponseBody
+
+  /** @return a function that will return response body decoded in the provided charset */
+  case class charset(set: Charset) extends (client.Response => String) {
+    def apply(r: client.Response) = r.getResponseBody(set.name)
+  }
+
+  /** @return a function that will return response body as a utf8 decoded string */
+  object utf8 extends charset(Charset.forName("utf8"))
 }
 
 object Bytes extends (client.Response => Array[Byte]) {
