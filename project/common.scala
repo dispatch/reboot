43fb3f0523db5f3d5ff7db8ef2ca1145eb397246
Nathan Hamblen <nathan@technically.us>
2014-08-10 18:41:31 -0400

    0.11.2

--- project/common.scala
+++ project/common.scala
@@ -13,9 +13,9 @@ object Common {
   )
 
   val settings: Seq[Setting[_]] = ls.Plugin.lsSettings ++ Seq(
-    version := "0.11.1",
+    version := "0.11.2",
 
-    crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.0"),
+    crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.2"),
 
     scalaVersion := defaultScalaVersion,
 
