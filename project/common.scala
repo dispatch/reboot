46979348f2390de7917dc3168875102d8372ffc1
Seth Tisue <seth@tisue.net>
2017-04-26 10:52:37 -0700

    upgrade Scala version from 2.12.1 to 2.12.2

--- project/common.scala
+++ project/common.scala
@@ -3,7 +3,7 @@ import sbt._
 object Common {
   import Keys._
 
-  val defaultScalaVersion = "2.12.1"
+  val defaultScalaVersion = "2.12.2"
 
   val testSettings:Seq[Setting[_]] = Seq(
     testOptions in Test += Tests.Cleanup { loader =>
@@ -15,7 +15,7 @@ object Common {
   val settings: Seq[Setting[_]] = ls.Plugin.lsSettings ++ Seq(
     version := "0.12.0",
 
-    crossScalaVersions := Seq("2.11.8", "2.12.1"),
+    crossScalaVersions := Seq("2.11.8", "2.12.2"),
 
     scalaVersion := defaultScalaVersion,
 
