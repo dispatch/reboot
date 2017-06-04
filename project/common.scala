432a3db2aa6151b8cee0888fafa5f4e8b6defe84
Matt Farmer <matt@frmr.me>
2017-06-04 11:58:50 -0400

    Bump version to 0.13.0-M2 for release

--- project/common.scala
+++ project/common.scala
@@ -13,7 +13,7 @@ object Common {
   )
 
   val settings: Seq[Setting[_]] = ls.Plugin.lsSettings ++ Seq(
-    version := "0.13.0-SNAPSHOT",
+    version := "0.13.0-M2",
 
     crossScalaVersions := Seq("2.11.8", "2.12.2"),
 
