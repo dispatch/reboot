d3ce81e58c3431429b8fcba920274eeb36756caf
Matt Farmer <matt@frmr.me>
2017-09-18 17:36:47 -0400

    Bump Scala version to 2.12.3

--- project/common.scala
+++ project/common.scala
@@ -3,7 +3,7 @@ import sbt._
 object Common {
   import Keys._
 
-  val defaultScalaVersion = "2.12.2"
+  val defaultScalaVersion = "2.12.3"
 
   val testSettings:Seq[Setting[_]] = Seq(
     testOptions in Test += Tests.Cleanup { loader =>
@@ -15,7 +15,7 @@ object Common {
   val settings: Seq[Setting[_]] = ls.Plugin.lsSettings ++ Seq(
     version := "0.13.2-SNAPSHOT",
 
-    crossScalaVersions := Seq("2.11.11", "2.12.2"),
+    crossScalaVersions := Seq("2.11.11", "2.12.3"),
 
     scalaVersion := defaultScalaVersion,
 
