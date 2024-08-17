94601477302154e0d0311d2e56d45860ac55e30d
Matt Farmer <matt@frmr.me>
2024-08-17 13:51:39 -0400

    Working compile on scala 3

--- project/common.scala
+++ project/common.scala
@@ -16,7 +16,7 @@ object Common {
   val settings: Seq[Setting[_]] = Seq(
     version := "2.0.0-SNAPSHOT",
 
-    crossScalaVersions := Seq("2.13.14"),
+    crossScalaVersions := Seq("2.13.14", "3.3.3"),
 
     scalaVersion := defaultScalaVersion,
 
